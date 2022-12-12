205149c4cc8e9954caace86ee988efaa74610f91
Adnan <102221244+losingsan3ty@users.noreply.github.com>
2022-12-12 15:37:35 +0300

    better file organization

--- root/frontend/webpack.config.js
+++ root/frontend/webpack.config.js
@@ -1,8 +1,11 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const dotenv = require("dotenv");
 // const BundleAnalyzerPlugin =
 // require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
-
+dotenv.config({
+  path: path.resolve(__dirname, "config.env"),
+});
 module.exports = {
   mode: "development",
   entry: { bundle: path.resolve(__dirname, "src/js/index.js") },
@@ -12,12 +15,14 @@ module.exports = {
     clean: true,
     assetModuleFilename: "[name][ext]",
   },
-  devtool: "source-map",
+  // devtool: "source-map",
   devServer: {
     static: { directory: path.resolve(__dirname, "dist") },
-    port: 3000,
-    open: true,
+    watchFiles: ["src/templates/*.html"],
     hot: true,
+    port: 3000,
+    open: true, //open the browser
+    hot: true, //reload
     compress: true,
     historyApiFallback: true,
   },
@@ -37,18 +42,32 @@ module.exports = {
           },
         },
       },
+
       {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        test: /\.(png|jpe?g|gif)$/i,
         type: "asset/resource",
+        generator: {
+          filename: "images/[name][ext]",
+        },
+        // generator: {
+        //   "asset/resource": {},
+        // },
+        // options: {
+        //   name: "[path][name].[ext]",
+        //   outputPath: "images",
+        // },
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: "Sora",
+      title: process.env.TITLE,
       filename: "index.html",
-      template: "src/templates/index-template.html",
+      template: path.resolve(__dirname, "src/templates/index-template.html"),
     }),
-    //   new BundleAnalyzerPlugin(),
   ],
 };
