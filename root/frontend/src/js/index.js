4f5bfeb0cf3aa34eac344443159a758a7ddde810
Adnan <102221244+losingsan3ty@users.noreply.github.com>
2023-02-07 00:19:24 +0300

    forth day

--- root/frontend/src/js/index.js
+++ root/frontend/src/js/index.js
@@ -1,1 +1,9 @@
 import "../styles/main.scss";
+const accordion = document.querySelector(".accordion");
+
+accordion.addEventListener("click", function (e) {
+  const accordionBtn = e.target.closest(".accordion-btn");
+  if (accordionBtn) {
+    accordionBtn.nextElementSibling.classList.toggle("hidden");
+  }
+});
