fdeb2e78c6016c410aac284a8d4ffd2585f20684
Adnan <102221244+losingsan3ty@users.noreply.github.com>
2023-02-11 23:49:26 +0300

    working on accordion

--- root/frontend/src/js/index.js
+++ root/frontend/src/js/index.js
@@ -4,6 +4,18 @@ const accordion = document.querySelector(".accordion");
 accordion.addEventListener("click", function (e) {
   const accordionBtn = e.target.closest(".accordion-btn");
   if (accordionBtn) {
-    accordionBtn.nextElementSibling.classList.toggle("hidden");
+    const accordionList = accordionBtn.nextElementSibling;
+    if (
+      accordionList.style.maxHeight === "0px" ||
+      !accordionList.style.maxHeight
+    ) {
+      accordionList.style.maxHeight = "500px";
+      accordionList.style.opacity = "2";
+      accordionList.style.marginBottom = "1rem";
+      return;
+    }
+    accordionList.style.maxHeight = "0px";
+    accordionList.style.opacity = "0";
+    accordionList.style.marginBottom = "0rem";
   }
 });
